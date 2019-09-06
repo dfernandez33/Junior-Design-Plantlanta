4f04d621fef29bf9f8a5c0c64ff26a9071df25c6
Luis Pastrana <pastrana@gatech.edu>
2019-09-06 15:21:40 -0400

    Initial iteration over home page

--- Flutter/lib/screens/verify_email.dart
+++ Flutter/lib/screens/verify_email.dart
@@ -5,11 +5,26 @@ import 'package:junior_design_plantlanta/widgets/progress_button.dart';
 class VerifyEmail extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(child: _buildCenteredVerification());
+    return Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          elevation: 10,
+          backgroundColor: Theme.of(context).primaryColor,
+          title: Text("Plantlanta"),
+        ),
+        body: PageView(
+            physics: NeverScrollableScrollPhysics(),
+            children: <Widget>[
+              Center(
+                child: _buildCenteredVerification(),
+              )
+            ]));
   }
 
   Widget _buildCenteredVerification() {
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.center,
       children: <Widget>[
         Text(
           "Please verify your email!",
