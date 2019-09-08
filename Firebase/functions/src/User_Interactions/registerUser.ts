d466c463a491c2690f285d20d06893697ac6d727
David Jose Fernandez <32881830+dfernandez33@users.noreply.github.com>
2019-09-08 17:27:17 -0400

    registration backend work

---
+++ Firebase/functions/src/User_Interactions/registerUser.ts
@@ -0,0 +1,33 @@
+import * as functions from "firebase-functions"
+import {ResponseCode} from "../Enums/responseCode";
+
+export const handler = function(data: userRegistrationInterface, context:functions.https.CallableContext, firestore: FirebaseFirestore.Firestore) {
+    let UUID;
+    if (context.auth !== undefined) {
+        UUID = context.auth.uid
+    } else {
+        return {
+            status: ResponseCode.FAILURE,
+            message: "No UUID received from context."
+        };
+    }
+    const userRef = firestore.collection("Users").doc(UUID);
+
+    return userRef.set({
+        name: data.name,
+        dob: new Date(Date.parse(data.dob)),
+        phone: data.phone,
+        address: data.address,
+        preferences: data.preferences
+    }).then(() => {
+        return {
+            status: ResponseCode.SUCCESS,
+            message: "User record created successfully"
+        }
+    }).catch((error) => {
+        return {
+            status: ResponseCode.FAILURE,
+            message: error
+        }
+    })
+}
