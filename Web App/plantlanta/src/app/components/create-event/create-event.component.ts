30b6329fd6afd093840ec8011aa3f8206f01d3e3
Simon Moreno <smoreno9@gatech.edu>
2019-09-29 18:08:38 -0400

    only missing spinner

--- Web App/plantlanta/src/app/components/create-event/create-event.component.ts
+++ Web App/plantlanta/src/app/components/create-event/create-event.component.ts
@@ -1,9 +1,8 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormGroup, FormControl } from '../../../../node_modules/@angular/forms';
-import { Router, ActivatedRoute } from '../../../../node_modules/@angular/router';
-import { AngularFireAuth } from '../../../../node_modules/@angular/fire/auth';
 import { AngularFireFunctions } from '../../../../node_modules/@angular/fire/functions';
 import { SpinnerComponent } from '../../widgets/spinner/spinner.component';
+import { Timestamp } from 'rxjs/internal/operators/timestamp';
 
 @Component({
   selector: 'app-create-event',
@@ -14,7 +13,6 @@ export class CreateEventComponent implements OnInit {
 
   validForm = true;
   errMessage = "";
-  badRequestId = false;
   eventCreated = false;
 
   @ViewChild('spinner', {static: false}) spin: SpinnerComponent;
@@ -24,19 +22,16 @@ export class CreateEventComponent implements OnInit {
   eventForm = new FormGroup({
     eventName: new FormControl(''),
     eventLocation: new FormControl(''),
-    eventDate: new FormControl(new Date()),
+    eventDate: new FormControl(''),
     eventStart: new FormControl(''),
     eventEnd: new FormControl(''),
     eventDescription: new FormControl(''),
   });
 
-  constructor(private afAuth: AngularFireAuth, private cloud: AngularFireFunctions, private router: Router, private route: ActivatedRoute) {}
+  constructor(private cloud: AngularFireFunctions) {}
 
   ngOnInit() {
     this.createEventFunction = this.cloud.httpsCallable("createEvent");
-    this.eventForm.valueChanges.subscribe(change => {
-      console.log(change)
-    })
   }
 
   async registerEvent() {
@@ -47,7 +42,7 @@ export class CreateEventComponent implements OnInit {
           name: formValues["eventName"],
           location: formValues["eventLocation"],
           description: formValues["eventDescription"],
-          date: formValues["eventDate"],
+          date: formValues["eventDate"].toString(),
           startTime: formValues["eventStart"],
           endTime: formValues["eventEnd"]
       }).toPromise();
@@ -70,5 +65,9 @@ export class CreateEventComponent implements OnInit {
       this.validForm = false;
       this.errMessage = "All fields are required to create an event. Please fill them out.";
     }
+  }
+
+  closeModal() {
+    this.eventCreated = false;
   }
 }
