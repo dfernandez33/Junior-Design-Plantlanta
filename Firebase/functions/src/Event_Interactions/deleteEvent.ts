d63e2a69a69b4266ba4a45ead53cb3ce40b48572
David Jose Fernandez <32881830+dfernandez33@users.noreply.github.com>
2019-10-08 00:05:06 -0400

    Update deleteEvent.ts

--- Firebase/functions/src/Event_Interactions/deleteEvent.ts
+++ Firebase/functions/src/Event_Interactions/deleteEvent.ts
@@ -2,5 +2,6 @@ import * as functions from 'firebase-functions';
 import { ResponseCode } from '../Enums/responseCode';
 
 export const handler = function(data: any, context: functions.https.CallableContext, firestore: FirebaseFirestore.Firestore) {
+    const eventId = data.eventId;
     
 }
