c50de9d4dd3c4ef98ccd1036f27ff0db7c3f6476
Nico Grande <nicogrande@gatech.edu>
2019-10-11 16:01:39 -0400

    Adding purchase item endpoint.

---
+++ Firebase/functions/src/Item_Interactions/purchaseItem.ts
@@ -0,0 +1,57 @@
+import * as functions from 'firebase-functions';
+import * as admin from "firebase-admin";
+import {ResponseCode} from "../Enums/responseCode";
+
+export const handler = async function(data: purchaseRequest, context: functions.https.CallableContext, firestore: FirebaseFirestore.Firestore) {
+    const itemID = data.ItemID;
+    let UUID;
+    if (context.auth !== undefined) {
+        UUID = context.auth.uid
+    } else {
+        return {
+            status: ResponseCode.FAILURE,
+            message: "No UUID received from context."
+        };
+    }
+
+    var batch = firestore.batch();
+
+    const itemRef = firestore.collection("Items").doc(itemID);
+    const item = await itemRef.get();
+    const itemData = item.data();
+    const userRef = firestore.collection("Users").doc(UUID);
+    const transactionRef = firestore.collection("Transactions").doc();
+        
+    batch.create(transactionRef, 
+        {
+            amount: -itemData.price,
+            timestamp: new Date(),
+            description: "Purchased " + itemData.name,
+            uuid: UUID
+        }
+    );
+
+    batch.update(itemRef, 
+        {
+            inventory: admin.firestore.FieldValue.increment(-1),
+        }
+    );
+
+    batch.update(userRef,
+        {
+            points: admin.firestore.FieldValue.increment(-itemData.price),
+        }
+    );
+
+    return batch.commit().then(() => {
+        return {
+            status: ResponseCode.SUCCESS,
+            message: "Success updating user and event info"
+        };
+    }).catch(() => {
+        return {
+            status: ResponseCode.FAILURE,
+            message: "Error updating user and event info"
+        };
+    });
+}
