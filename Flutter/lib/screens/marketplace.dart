d3edfa93fa02268e7f75c03ec8945c73aa0b8a46
David Jose Fernandez <32881830+dfernandez33@users.noreply.github.com>
2019-11-08 14:56:16 -0500

    add filtering to events and marketplace

--- Flutter/lib/screens/marketplace.dart
+++ Flutter/lib/screens/marketplace.dart
@@ -1,16 +1,14 @@
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:algolia/algolia.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:junior_design_plantlanta/model/item_model.dart';
 import 'package:junior_design_plantlanta/model/user.dart';
-import 'package:junior_design_plantlanta/services/user_service.dart';
+import 'package:junior_design_plantlanta/services/algolia_service.dart';
 import 'package:junior_design_plantlanta/widgets/item_card.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Marketplace extends StatefulWidget {
   UserModel userData;
-  String queryText;
-
 
   Marketplace(this.userData);
 
@@ -22,17 +20,29 @@ class Marketplace extends StatefulWidget {
 // TODO: Improve Progress Dialog to respond to error/success.
 class _MarketplaceState extends State<Marketplace> {
 
+  bool isLoading;
   Future<dynamic> itemStream;
   List<ItemModel> items;
-  
+  List rawItems;
+  AlgoliaIndexReference algolia = AlgoliaService.algolia.instance.index('Items');
+
+
   _MarketplaceState() {
     items = List();
+    rawItems = List();
     this.itemStream = _getItems();
   }
 
   @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    isLoading = true;
+  }
+
+  @override
   Widget build(BuildContext context) {
-    if (items.isEmpty || widget.userData == null) {
+    if (isLoading) {
       return Scaffold(
         body: Center(
             child: CircularProgressIndicator(
@@ -42,6 +52,27 @@ class _MarketplaceState extends State<Marketplace> {
         )),
       );
     } else {
+      Widget content;
+      if (this.items.length == 0) {
+        content = Center(
+          child: Text("No Items Found"),
+        );
+      } else {
+        content = Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: StaggeredGridView.countBuilder(
+            crossAxisCount: 4,
+            itemCount: this.items.length,
+            itemBuilder: (BuildContext context, int index) =>
+                ItemCard(this.items[index], this.widget.userData),
+            staggeredTileBuilder: (int index) {
+              return StaggeredTile.fit(2);
+            },
+            mainAxisSpacing: 4.0,
+            crossAxisSpacing: 4.0,
+          ),
+        );
+      }
       return Scaffold(
           appBar: new AppBar(
               backgroundColor: Theme.of(context).backgroundColor,
@@ -50,32 +81,20 @@ class _MarketplaceState extends State<Marketplace> {
                 TextField(
                   cursorColor: Theme.of(context).primaryColor,
                   decoration: InputDecoration(
-                      hintText: 'Enter a search term'
+                      hintText: 'Search items by name or brand'
                   ),
                   onChanged: (text) {
-                    widget.queryText = text;
-
+                    _updateFilteredItems(text);
                   },
                 ),
               ])),
-          body: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: StaggeredGridView.countBuilder(
-              crossAxisCount: 4,
-              itemCount: this.items.length,
-              itemBuilder: (BuildContext context, int index) =>
-                  ItemCard(this.items[index], this.widget.userData),
-              staggeredTileBuilder: (int index) {
-                return StaggeredTile.fit(2);
-              },
-              mainAxisSpacing: 4.0,
-              crossAxisSpacing: 4.0,
-            ),
-          ));
+          body: content,
+      );
     }
   }
 
   Future<dynamic> _getItems() async {
+    this.isLoading = true;
     return Firestore.instance
         .collection("Items")
         .getDocuments()
@@ -85,8 +104,20 @@ class _MarketplaceState extends State<Marketplace> {
       setState(() {
         items.forEach((item) {
           item.data.addEntries([MapEntry("itemID", item.reference.documentID)]);
+          this.rawItems.add(item.data);
           this.items.add(ItemModel.fromJson(item.data));
         });
+        this.isLoading = false;
+      });
+    });
+  }
+
+  _updateFilteredItems(String query) {
+    this.algolia.search(query).getObjects().then((results) {
+      List hitIDs = results.hits.map((hit) => hit.objectID).toList();
+      List updatedItems = this.rawItems.where((item) => hitIDs.contains(item['itemID'])).toList();
+      setState(() {
+        this.items = updatedItems.map((item) => ItemModel.fromJson(item)).toList();
       });
     });
   }
