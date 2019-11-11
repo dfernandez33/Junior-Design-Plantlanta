153ed0ce9f62a0580a670c4544241a78f59ab05b
Nico Grande <nicogrande@gatech.edu>
2019-11-11 00:32:25 -0500

    doesnt work.

--- Flutter/lib/screens/profile.dart
+++ Flutter/lib/screens/profile.dart
@@ -1,12 +1,13 @@
+import 'package:built_collection/built_collection.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:junior_design_plantlanta/model/event_model.dart';
 import 'package:junior_design_plantlanta/model/user.dart';
+import 'package:junior_design_plantlanta/widgets/past_event_card.dart';
 
-enum ProfileTab {
-  UPCOMING_EVENTS,
-  PAST_EVENTS,
-  TRANSACTIONS
-}
+enum ProfileTab { UPCOMING_EVENTS, PAST_EVENTS, TRANSACTIONS }
 
 class Profile extends StatefulWidget {
   UserModel _user;
@@ -20,81 +21,80 @@ class Profile extends StatefulWidget {
 class _ProfileState extends State<Profile> {
   ProfileTab _tabSelected = ProfileTab.UPCOMING_EVENTS;
 
+  List<PastEventCard> pastEvents = List();
+
   @override
   Widget build(BuildContext context) {
     if (widget._user == null) {
       return Scaffold(
         body: Center(
             child: CircularProgressIndicator(
-              value: null,
-              valueColor:
+          value: null,
+          valueColor:
               AlwaysStoppedAnimation<Color>(Theme.of(context).primaryColor),
-            )),
+        )),
       );
     } else {
       return Scaffold(
           body: ListView(
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              children: <Widget>[
+                Row(
                   children: <Widget>[
-                    Row(
-                      children: <Widget>[
-                        CircleAvatar(
-                          radius: 40.0,
-                          backgroundColor: Colors.grey,
-                          backgroundImage: NetworkImage("https://icon-library.net/icon/default-profile-icon-24.html"),
-                        ),
-                        Expanded(
-                          flex: 1,
-                          child: Column(
+                    CircleAvatar(
+                      radius: 40.0,
+                      backgroundColor: Colors.grey,
+                    ),
+                    Expanded(
+                      flex: 1,
+                      child: Column(
+                        children: <Widget>[
+                          Row(
+                            mainAxisSize: MainAxisSize.max,
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                             children: <Widget>[
-                              Row(
-                                mainAxisSize: MainAxisSize.max,
-                                mainAxisAlignment:
-                                MainAxisAlignment.spaceEvenly,
-                                children: <Widget>[
-                                  Text("POST"),
-                                  Text("EVENTS"),
-                                  Text("DUDES"),
-                                ],
-                              ),
+                              Text("POST"),
+                              Text("EVENTS"),
+                              Text("DUDES"),
+                            ],
+                          ),
 //                        Row(
 //                            mainAxisAlignment:
 //                            MainAxisAlignment.spaceEvenly,
 //                            children: <Widget>[
 //                              buildProfileFollowButton(user)
 //                            ]),
-                            ],
-                          ),
-                        )
-                      ],
-                    ),
-                    Container(
-                        alignment: Alignment.centerLeft,
-                        padding: const EdgeInsets.only(top: 15.0),
-                        child: Text(
-                          widget._user.name,
-                          style: TextStyle(fontWeight: FontWeight.bold),
-                        )),
+                        ],
+                      ),
+                    )
                   ],
                 ),
+                Container(
+                    alignment: Alignment.centerLeft,
+                    padding: const EdgeInsets.only(top: 15.0),
+                    child: Text(
+                      widget._user.name,
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    )),
+              ],
+            ),
+          ),
+          buildImageViewButtonBar(),
+          Stack(
+            children: <Widget>[
+              Container(
+                height: MediaQuery.of(context).size.width,
+                color: Theme.of(context).primaryColor,
               ),
-              buildImageViewButtonBar(),
-              Stack(
-                children: <Widget>[
-                  Container(
-                    height: MediaQuery.of(context).size.width,
-                    color: Theme.of(context).primaryColor,
-                  ),
-                  _buildTabContent(),
-                ],
-              ),
-//        buildSelection(),
+              _buildTabContent(),
             ],
-          )
-      );
+          ),
+//        buildSelection(),
+        ],
+      ));
     }
   }
 
@@ -102,7 +102,23 @@ class _ProfileState extends State<Profile> {
     if (this._tabSelected == ProfileTab.UPCOMING_EVENTS) {
       return Text("ProfileTab.UPCOMING_EVENTS");
     } else if (this._tabSelected == ProfileTab.PAST_EVENTS) {
-      return Text("ProfileTab.PAST_EVENTS");
+
+      if (this.pastEvents.isEmpty) {
+        _buildPastEventCards();
+        return Center(
+          child: CircularProgressIndicator(
+            value: null,
+            valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+          ),
+        );
+      } else {
+        Container(
+            margin: EdgeInsets.only(top: 12.0),
+            child: ListView(
+              children: pastEvents,
+              shrinkWrap: true,
+            ));
+      }
     } else if (this._tabSelected == ProfileTab.TRANSACTIONS) {
       return Text("ProfileTab.TRANSACTIONS");
     } else {
@@ -111,11 +127,11 @@ class _ProfileState extends State<Profile> {
   }
 
   Widget backgroundLayer() {
-      Container(
-        width: double.infinity,
-        height: double.infinity,
-        color: Theme.of(context).primaryColor,
-      );
+    Container(
+      width: double.infinity,
+      height: double.infinity,
+      color: Theme.of(context).primaryColor,
+    );
   }
 
   Row buildImageViewButtonBar() {
@@ -129,13 +145,11 @@ class _ProfileState extends State<Profile> {
     );
   }
 
-
   void _tabSelector(ProfileTab newState) {
     setState(() {
       this._tabSelected = newState;
     });
   }
-
 
   Widget _buildProperTab(IconData icon, double size, ProfileTab state) {
     if (state == this._tabSelected) {
@@ -154,7 +168,6 @@ class _ProfileState extends State<Profile> {
             ),
           ),
         ),
-
       );
     } else {
       return Expanded(
@@ -174,8 +187,30 @@ class _ProfileState extends State<Profile> {
       );
     }
   }
+
+  Future<void> _buildPastEventCards() async {
+    widget._user.confirmedEvents.forEach((event) async {
+      var eventInfo =
+          await Firestore.instance.collection("Events").document(event).get();
+      Timestamp timestamp = eventInfo.data['date'];
+
+      var tempTime = <String, dynamic>{
+        "_nanoseconds": timestamp.nanoseconds,
+        "_seconds": timestamp.seconds
+      };
+
+      eventInfo.data['date'] = tempTime;
+      setState(() {
+        this.pastEvents.add(PastEventCard(EventModel.fromJson(eventInfo.data)));
+      });
+    });
+  }
+
   Widget _getImage() {
     return Image.asset(
-      'assets/add_profile_picture.png', fit: BoxFit.fitHeight, color: Colors.grey[400],);
+      'assets/add_profile_picture.png',
+      fit: BoxFit.fitHeight,
+      color: Colors.grey[400],
+    );
   }
 }
---
+++ Flutter/lib/widgets/past_event_card.dart
@@ -0,0 +1,203 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_functions/cloud_functions.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+import 'package:junior_design_plantlanta/serializers/StatusResponse.dart';
+import 'package:junior_design_plantlanta/model/event_model.dart';
+
+//TODO: Refactor UI
+class PastEventCard extends StatefulWidget {
+  EventModel _model;
+  String userId;
+
+  PastEventCard(this._model) {
+    // TODO: Provide a context in global scope
+    FirebaseAuth.instance.currentUser().then((userId) =>
+    this.userId = userId.uid);
+
+  }
+
+  // TODO: Find a controller to update the model in a better way.
+  @override
+  _PastEventCardState createState() =>
+      _PastEventCardState(_model.participants.contains(userId));
+}
+
+class _PastEventCardState extends State<PastEventCard> {
+  bool isExpanded = false;
+  bool isSignUp;
+
+  _PastEventCardState(this.isSignUp);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(15.0))),
+      elevation: 2,
+      clipBehavior: Clip.antiAlias,
+      margin: EdgeInsets.only(left:12.0, right: 12.0, bottom: 12.0),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ExpansionTile(
+            onExpansionChanged: (bool expanding) => setState(() => this.isExpanded = expanding),
+            backgroundColor: Colors.white,
+            title: _buildTitle(),
+            children: <Widget>[_buildContent(),]
+        ),
+      ),
+    );
+  }
+
+  Widget _buildTitle() {
+    return Container(
+        child: Column(
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Container(
+                    padding: EdgeInsets.only(bottom: 5.0),
+                    child: Text(widget._model.name,
+                      textAlign: TextAlign.start,
+                      style: TextStyle(
+                          color: isExpanded ?
+                          Theme.of(context).primaryColor : Colors.black54,
+                          fontSize: 20.0),
+
+                    )
+                )
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                Column(
+                    children: <Widget>[
+                      Text(_getDate(DateTime.fromMillisecondsSinceEpoch(widget._model.date.seconds * 1000)),
+                          textAlign: TextAlign.start,
+                          style: TextStyle(
+                              color: Colors.black38,
+                              fontSize: 16.0)
+                      )]
+                )
+              ],
+            ),
+          ],
+        )
+    );
+  }
+
+  Widget _buildContent() {
+    return Container(
+        padding: const EdgeInsets.only(left: 16.0, right: 16.0),
+        child: Column(
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Container(
+                  child: Text("Location: ",
+                      style: TextStyle(
+                          color: Colors.black54,
+                          fontSize: 16.0)),
+                ),
+                Container(
+                  child: Text(widget._model.location,
+                      style: TextStyle(
+                          color: Colors.black54,
+                          fontSize: 16.0)),
+                )
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                Container(
+                  child: Text("Start Time: ",
+                      style: TextStyle(
+                          color: Colors.black54,
+                          fontSize: 16.0)),
+                ),
+                Container(
+                  child: Text(widget._model.startTime,
+                      style: TextStyle(
+                          color: Colors.black54,
+                          fontSize: 16.0)),
+                )
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                Container(
+                  child: Text("End Time: ",
+                      style: TextStyle(
+                          color: Colors.black54,
+                          fontSize: 16.0)),
+                ),
+                Container(
+                  child: Text(widget._model.endTime,
+                      style: TextStyle(
+                          color: Colors.black54,
+                          fontSize: 16.0)),
+                )
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                Expanded(
+                    child: Container(
+                      padding: const EdgeInsets.only(top: 4.0),
+                      child: Text(widget._model.description,
+                          style: TextStyle(
+                              color: Colors.black54,
+                              fontSize: 14.0)),
+                    )),
+              ],
+            ),
+          ],
+        )
+    );
+  }
+
+  String _getDate(DateTime date) {
+    return date.month.toString()
+        + "/" + date.day.toString()
+        + "/" + date.year.toString();
+  }
+
+
+  void _removeUserFromEvent() async {
+    // Are we fine by rebuilding the model in the component?
+    final HttpsCallable callable = CloudFunctions.instance.getHttpsCallable(
+      functionName: 'removeUserFromEvents',
+    );
+    try {
+      final HttpsCallableResult result = await callable.call(
+        <String, dynamic>{
+          'EventID': widget._model.eventId,
+        },
+      );
+      isSignUp = false;
+      StatusResponse resp = new StatusResponse.fromJson(result.data);
+      print(resp.message);
+
+    } catch (e) {
+      print(e.message);
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          // return object of type Dialog
+          return AlertDialog(
+            title: new Text("${e.message}"),
+            actions: <Widget>[
+              // usually buttons at the bottom of the dialog
+              new FlatButton(
+                child: new Text("Close"),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          );
+        },
+      );
+    }
+  }
+}
